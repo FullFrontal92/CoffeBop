31ce701a726de0a1d5d4c82e9190f47bb930c238
FullFrontal92 <adam.benyahia@hotmail.fr>
2018-09-28 22:11:24 +0200

    finish with drink

--- app/src/main/java/com/example/adambenyahia/coffeebop/DrinkCategoryActivity.java
+++ app/src/main/java/com/example/adambenyahia/coffeebop/DrinkCategoryActivity.java
@@ -1,7 +1,9 @@
 package com.example.adambenyahia.coffeebop;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -15,5 +17,12 @@ public class DrinkCategoryActivity extends ListActivity {
         ArrayAdapter<Drinks> listAdapter = new ArrayAdapter<Drinks>(this,android.R.layout.simple_list_item_1,Drinks.drinks);
 
         listDrinks.setAdapter(listAdapter);
+    }
+
+    public void onListItemClick(ListView listView, View itemView, int position, long id){
+
+        Intent intent = new Intent(DrinkCategoryActivity.this,DrinkActivity.class);
+        // intent.putExtra(DrinkActivity.EXTRA_DRINKNO, (int) id);
+        startActivity(intent);
     }
 }
